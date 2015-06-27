8763bfd320d8312cbe4877327264a428dacfef6b
Miguel Moldes <miguel.moldes@wooga.net>
2015-06-27 22:51:42 +0200

    Auto layout for Add words view

---
+++ REDVocabulary/modules/addWords/REDAddWordsInputView.swift
@@ -0,0 +1,17 @@
+//
+// Created by MIGUEL MOLDES on 21/6/15.
+// Copyright (c) 2015 MIGUEL MOLDES. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class REDAddWordsInputView : UIView {
+    
+    @IBOutlet weak var textView : UITextView!
+    @IBOutlet weak var languageLabel : UILabel!
+    @IBOutlet weak var coverView : UIView!
+    
+    
+
+}
---
+++ REDVocabulary/modules/addWords/REDAddWordsViewController.swift
@@ -0,0 +1,20 @@
+//
+// Created by MIGUEL MOLDES on 21/6/15.
+// Copyright (c) 2015 MIGUEL MOLDES. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class REDAddWordsViewController : ViewController {
+    
+    
+    @IBOutlet weak var containerView: UIView!
+    @IBOutlet weak var input1View: REDAddWordsInputView!
+    @IBOutlet weak var input2View: REDAddWordsInputView!
+    @IBOutlet weak var startButton: REDBasicButton!
+    
+    @IBOutlet weak var addButton: REDBasicButton!
+
+
+}

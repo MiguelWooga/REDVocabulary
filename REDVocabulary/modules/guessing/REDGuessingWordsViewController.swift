158698dfefb74e5bf680863e6d22153e33b0dfa7
Miguel Moldes <miguel.moldes@wooga.net>
2015-07-13 07:37:41 +0200

    More layout for guessing words

--- REDVocabulary/modules/guessing/REDGuessingWordsViewController.swift
+++ REDVocabulary/modules/guessing/REDGuessingWordsViewController.swift
@@ -7,5 +7,17 @@ import Foundation
 import UIKit
 
 class REDGuessingWordsViewController : ViewController {
+    
+    @IBOutlet weak var container: UIView!
+    @IBOutlet weak var progressLabel: UILabel!
+    @IBOutlet weak var firstLngSwitch: UILabel!
+    @IBOutlet weak var secondLngSwitch: UILabel!
+    @IBOutlet weak var switchBtn: UIButton!
+    @IBOutlet weak var inputOne: REDInputView!
+    @IBOutlet weak var inputTwo: REDInputView!
+    @IBOutlet weak var incorrectBtn: REDBasicButton!
+    
+    @IBOutlet weak var correctBtn: REDBasicButton!
+    
 
 }
